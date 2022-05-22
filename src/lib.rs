7b4a748181579b892ef8b659108034c4c15c0d22
Jason <jason.wiemels@gmail.com>
2022-05-22 02:00:39 -0700

    small memory improvements and cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod utils;
-
+#[cfg(feature = "console_error_panic_hook")]
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -7,6 +7,24 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
 
 #[derive(Clone)]
 #[wasm_bindgen]
@@ -23,17 +41,6 @@ pub struct Node {
 static ON: Node = Node{ a: None, b: None, c: None, d: None, population: 1, level: 0, hash: 1 };
 static OFF: Node = Node{ a: None, b: None, c: None, d: None, population: 0, level: 0, hash: 0 };
 
-trait OptionExt {
-    type Value;
-    fn unwrap_ref(&self) -> &Self::Value;
-    fn unwrap_mut(&mut self) -> &mut Self::Value;
-}
-impl <T> OptionExt for Option<T> {
-    type Value = T;
-    fn unwrap_ref(&self) -> &T { self.as_ref().unwrap() }
-    fn unwrap_mut(&mut self) -> &mut T { self.as_mut().unwrap() }
-}
-
 #[wasm_bindgen]
 impl Node {
     pub fn hash(&self) -> u64 {
@@ -45,21 +52,21 @@ impl Node {
     pub fn level(&self) -> u32 {
         self.level
     }
-    pub fn a(&self) -> Node {
-        *self.a.unwrap_ref().clone()
+    fn a(&self) -> Node {
+        *self.a.as_ref().unwrap().clone()
     }
-    pub fn b(&self) -> Node {
-        *self.b.unwrap_ref().clone()
+    fn b(&self) -> Node {
+        *self.b.as_ref().unwrap().clone()
     }
-    pub fn c(&self) -> Node {
-        *self.c.unwrap_ref().clone()
+    fn c(&self) -> Node {
+        *self.c.as_ref().unwrap().clone()
     }
-    pub fn d(&self) -> Node {
-        *self.d.unwrap_ref().clone()
+    fn d(&self) -> Node {
+        *self.d.as_ref().unwrap().clone()
     }
 }
 
-fn join(a: &Node, b: &Node, c: &Node, d: &Node) -> Node {
+fn join(a: Node, b: Node, c: Node, d: Node) -> Node {
     let n_level = a.level() + 1;
     let n_population: u32 = a.population() + b.population() + c.population() + d.population();
     let n_hash: u64 = (
@@ -71,10 +78,10 @@ fn join(a: &Node, b: &Node, c: &Node, d: &Node) -> Node {
     ) & ((1 << 63) - 1);
     
     Node {
-        a: Some(Box::new(a.clone())),
-        b: Some(Box::new(b.clone())),
-        c: Some(Box::new(c.clone())),
-        d: Some(Box::new(d.clone())),
+        a: Some(Box::new(a)),
+        b: Some(Box::new(b)),
+        c: Some(Box::new(c)),
+        d: Some(Box::new(d)),
         population: n_population,
         level: n_level,
         hash: n_hash,
@@ -87,28 +94,28 @@ fn get_zero(k: u32) -> Node {
     }
     else {
         join (
-            &get_zero(k - 1),
-            &get_zero(k - 1),
-            &get_zero(k - 1),
-            &get_zero(k - 1),
+            get_zero(k - 1),
+            get_zero(k - 1),
+            get_zero(k - 1),
+            get_zero(k - 1),
         )
     }
 }
 
-fn center(m: &Node) -> Node {
+fn center(m: Node) -> Node {
     let zero = get_zero(m.level() - 1);
     join (
-        &join(&zero, &zero, &zero, &m.a()),
-        &join(&zero, &zero, &m.b(), &zero),
-        &join(&zero, &m.c(), &zero, &zero),
-        &join(&m.d(), &zero, &zero, &zero),
+        join(zero.clone(), zero.clone(), zero.clone(), m.a()),
+        join(zero.clone(), zero.clone(), m.b(),zero.clone()),
+        join(zero.clone(), m.c(), zero.clone(),zero.clone()),
+        join(m.d(), zero.clone(), zero.clone(),zero),
     )
 }
 
-fn life(a: &Node, b: &Node, c: &Node, d: &Node, e: &Node, 
-        f: &Node, g: &Node, h: &Node, i: &Node) -> Node {
+fn life(a: Node, b: Node, c: Node, d: Node, e: Node, 
+        f: Node, g: Node, h: Node, i: Node) -> Node {
     let mut outer = 0;
-    for &n in [a, b, c, d, f, g, h, i].iter() {
+    for n in [a, b, c, d, f, g, h, i].iter() {
         if n.population() > 0 {
             outer += 1;
         }
@@ -126,28 +133,28 @@ fn life(a: &Node, b: &Node, c: &Node, d: &Node, e: &Node,
         _ => OFF.clone()
     }
 }
-fn life_4x4(m: &Node) -> Node {
+fn life_4x4(m: Node) -> Node {
     let a = m.a();
     let b = m.b();
     let c = m.c();
     let d = m.d();
 
-    let ab = life(&a.a(), &a.b(), &b.a(), &a.c(), &a.d(), &b.c(), &c.a(), &c.b(), &d.a());
-    let bc = life(&a.b(), &b.a(), &b.b(), &a.d(), &b.c(), &b.d(), &c.b(), &d.a(), &d.b());
-    let cb = life(&a.c(), &a.d(), &b.c(), &c.a(), &c.b(), &d.a(), &c.c(), &c.d(), &d.c());
-    let da = life(&a.d(), &b.c(), &b.d(), &c.b(),&d.a(), &d.b(), &c.d(), &d.c(), &d.d());
+    let ab = life(a.a(), a.b(), b.a(), a.c(),a.d(), b.c(), c.a(), c.b(), d.a());
+    let bc = life(a.b(), b.a(), b.b(), a.d(),b.c(), b.d(), c.b(), d.a(), d.b());
+    let cb = life(a.c(), a.d(), b.c(), c.a(),c.b(), d.a(), c.c(), c.d(), d.c());
+    let da = life(a.d(), b.c(), b.d(), c.b(),d.a(), d.b(), c.d(), d.c(), d.d());
 
     join(
-        &ab,
-        &bc,
-        &cb,
-        &da,
+        ab,
+        bc,
+        cb,
+        da,
     )
 }
 
-fn successor(m: &Node, j: Option<u32>) -> Node {
+fn successor(m: Node, j: Option<u32>) -> Node {
     if m.level() == 0 {
-        m.a().clone()
+        m.a()
     }
     else if m.level() == 2 {
         life_4x4(m)
@@ -161,35 +168,35 @@ fn successor(m: &Node, j: Option<u32>) -> Node {
             nj = Some(std::cmp::min(j.unwrap(), m.level() - 2));
         }
 
-        let a = &m.a();
-        let b = &m.b();
-        let c = &m.c();
-        let d = &m.d();
+        let a = m.a();
+        let b = m.b();
+        let c = m.c();
+        let d = m.d();
 
-        let c1 = successor(&join(&a.a(), &a.b(), &a.c(), &a.d()), nj);
-        let c2 = successor(&join(&a.b(), &b.a(), &a.d(), &b.c()), nj);
-        let c3 = successor(&join(&b.a(), &b.b(), &b.c(), &b.d()), nj);
-        let c4 = successor(&join(&a.c(), &a.d(), &c.a(), &c.b()), nj);
-        let c5 = successor(&join(&a.d(), &b.c(), &c.b(), &d.a()), nj);
-        let c6 = successor(&join(&b.c(), &b.d(), &d.a(), &d.b()), nj);
-        let c7 = successor(&join(&c.a(), &c.b(), &c.c(), &c.d()), nj);
-        let c8 = successor(&join(&c.b(), &d.a(), &c.d(), &d.c()), nj);
-        let c9 = successor(&join(&d.a(), &d.b(), &d.c(), &d.d()), nj);
+        let c1 = successor(join(a.a(), a.b(), a.c(), a.d()), nj);
+        let c2 = successor(join(a.b(), b.a(), a.d(), b.c()), nj);
+        let c3 = successor(join(b.a(), b.b(), b.c(), b.d()), nj);
+        let c4 = successor(join(a.c(), a.d(), c.a(), c.b()), nj);
+        let c5 = successor(join(a.d(), b.c(), c.b(), d.a()), nj);
+        let c6 = successor(join(b.c(), b.d(), d.a(), d.b()), nj);
+        let c7 = successor(join(c.a(), c.b(), c.c(), c.d()), nj);
+        let c8 = successor(join(c.b(), d.a(), c.d(), d.c()), nj);
+        let c9 = successor(join(d.a(), d.b(), d.c(), d.d()), nj);
 
         if nj.unwrap() < m.level() - 2 {
             join(
-                &join(&c1.d(), &c2.c(), &c4.b(), &c5.a()),
-                &join(&c2.d(), &c3.c(), &c5.b(), &c6.a()),
-                &join(&c4.d(), &c5.c(), &c7.b(), &c8.a()),
-                &join(&c5.d(), &c6.c(), &c8.b(), &c9.a())
+                join(c1.d(), c2.c(), c4.b(), c5.a()),
+                join(c2.d(), c3.c(), c5.b(), c6.a()),
+                join(c4.d(), c5.c(), c7.b(), c8.a()),
+                join(c5.d(), c6.c(), c8.b(), c9.a())
             )
         }
         else {
             join(
-                &successor(&join(&c1, &c2, &c4, &c5), nj),
-                &successor(&join(&c2, &c3, &c5, &c6), nj),
-                &successor(&join(&c4, &c5, &c7, &c8), nj),
-                &successor(&join(&c5, &c6, &c8, &c9), nj)
+                successor(join(c1, c2.clone(), c4.clone(), c5.clone()), nj),
+                successor(join(c2, c3, c5.clone(), c6.clone()), nj),
+                successor(join(c4, c5.clone(), c7, c8.clone()), nj),
+                successor(join(c5, c6, c8, c9), nj)
             )
         }
     }
@@ -197,7 +204,7 @@ fn successor(m: &Node, j: Option<u32>) -> Node {
 
 #[wasm_bindgen]
 impl Node {
-    pub fn advance(node: &Node, mut n: u32) -> Node {
+    pub fn advance(node: Node, mut n: u32) -> Node {
         if n == 0 {
             return node.clone();
         }
@@ -207,13 +214,13 @@ impl Node {
         while n > 0 {
             bits.push(n & 1);
             n = n >> 1;
-            node = center(&node);
+            node = center(node);
         }
     
         for (k, bit) in bits.iter().rev().enumerate() {
             let j: u32 = (bits.iter().len() - k - 1).try_into().unwrap();
             if bit != &0 {
-                node = successor(&node, Some(j));
+                node = successor(node, Some(j));
             }
         }
         node
@@ -222,14 +229,17 @@ impl Node {
     pub fn ffwd(node: &Node, n: u32) -> Node {
         let mut node = node.clone();
         for _ in 0..n {
-            while node.level() < 3 || 
-                node.a().level() != node.a().d().d().level() ||
-                node.b().level() != node.b().c().c().level() ||
-                node.c().level() != node.c().b().b().level() ||
-                node.d().level() != node.d().a().a().level() {
-                node = center(&node);
-            }
-            node = successor(&node, None);
+            // while node.level() < 3 || 
+            //     node.a().population() != node.a().d().d().population() ||
+            //     node.b().population() != node.b().c().c().population() ||
+            //     node.c().population() != node.c().b().b().population() ||
+            //     node.d().population() != node.d().a().a().population() {
+            //     log("looping begin");
+            //     node = center(node);
+            //     log("looping end");
+            // }
+            log("done looping");
+            node = successor(node, None);
         }
         node
     }
@@ -278,12 +288,15 @@ impl Node {
         }
 
         let mut k = 0;
-        let mut last_updated: Node = ON.clone();
+        let mut last_updated = (0, 0);
         while pattern.len() != 1 {
             let mut next_level = std::collections::HashMap::new();
             let z = get_zero(k);
 
             while pattern.len() > 0 {
+
+                log("construction begin");
+
                 let (mut x, mut y) = pattern.iter().next().unwrap().0;
                 x = x - (x & 1);
                 y = y - (y & 1);
@@ -310,13 +323,18 @@ impl Node {
                     d = z.clone();
                 }
 
-                *next_level.entry((x >> 1, y >> 1)).or_insert(z.clone()) = join(&a, &b, &c, &d);
-                last_updated = next_level[&(x >> 1, y >> 1)].clone();
+                log("weird step1");
+                last_updated = (x >> 1, y >> 1);
+
+                log("weird step2");
+                next_level.insert((x >> 1, y >> 1), join(a, b, c, d));
+
+                log(next_level.len().to_string().as_str());
             }
             pattern = next_level;
             k += 1;
         }
-        last_updated
+        pattern[&last_updated].clone()
     }
 }
 
--- www/index.js
+++ www/index.js
@@ -18,15 +18,28 @@ console.log(node.population());
 
 console.log("new population: ", node.population());
 
-const button = document.getElementById("create");
-
-button.addEventListener("click", () => {
+const create = document.getElementById("create");
+create.addEventListener("click", () => {
   const items1 = [1, 1, 2, 2, 3, 3, 4, 4, -1, -1, -2, -2, -3, -3, -400, -400, 500, 500];
   const node1 = Node.construct(items1);
   console.log(node1.level());
   console.log(node1.population());
   const items2 = Node.expand(node1, 0, 0);
   console.log(items2);
+
+  // Node.ffwd(node1, 10);
+  // console.log(node1.level());
+  // console.log(node1.population());
+})
+
+const forward = document.getElementById("forward");
+forward.addEventListener("click", () => {
+  Node.ffwd(node, 7);
+  console.log(node.level());
+  console.log(node.population());
+
+  const items = Node.expand(node, 0, 0);
+  console.log(items);
 
   // Node.ffwd(node1, 10);
   // console.log(node1.level());
